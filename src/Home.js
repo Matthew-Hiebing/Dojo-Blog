11bcd82c2ec1a2037eff4038a4a8a5a571ebb65d
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-01 13:33:26 -0600

    feat: Practice using setstate hooks.

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+    // let name = "Matt";
+    const [name, setName] = useState('Matt');
+    const [age, setAge] = useState('30');
 
-    const handleClick = (e) => {
-        console.log('hello ninjas', e)
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello' + name, e.target)
+    const handleClick = () => {
+        setName('luigi');
+        setAge(25);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old.</p>
+            <p>{ name }</p>
             <button onClick={ handleClick }>Click me</button>
-            <button onClick={ (e) => handleClickAgain(' Matt', e)}>Click me again</button>
         </div>
     );
 }
