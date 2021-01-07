db675d9fc52944652ed223614a7409c4f5f67a0d
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-07 13:11:09 -0600

    feat: handleDelete() only runs once for each name.
    If you click, "change name" the function will run again because the name is being passed into the useEffect() function as a dependency.

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -8,14 +8,24 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
     ])
 
+    const[name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    // useEffect() allows you to run the handleDelete function once.
+    useEffect(() => {
+        console.log('useEffect ran')
+        console.log(name)
+    }, [name]);
+
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName('luigi')}>chnage name</button>
+            <p>{ name }</p>
         </div>
     );
 }
