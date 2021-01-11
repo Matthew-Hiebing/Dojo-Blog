0ac773565dcf5e8654d3c209043c66a59c07c0e5
Hiebs915 <Matthew.Hiebing@gmail.com>
2021-01-11 08:33:04 -0600

    feat: Creat mock endpoint using JSON data file.

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,6 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
     ])
 
-    const[name, setName] = useState('mario');
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -18,14 +16,11 @@ const Home = () => {
     // useEffect() allows you to run the handleDelete function once.
     useEffect(() => {
         console.log('useEffect ran')
-        console.log(name)
-    }, [name]);
+    }, []);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            <button onClick={() => setName('luigi')}>chnage name</button>
-            <p>{ name }</p>
         </div>
     );
 }
